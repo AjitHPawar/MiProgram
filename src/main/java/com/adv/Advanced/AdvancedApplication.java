f6d26c1c12bfa5980930e8ccbfac2aa74ccaab56
ajit <ajinathp.guru@gmail.com>
2023-05-23 13:44:08 +0530

    second commit

--- src/main/java/com/adv/Advanced/AdvancedApplication.java
+++ src/main/java/com/adv/Advanced/AdvancedApplication.java
@@ -2,12 +2,20 @@ package com.adv.Advanced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AdvancedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String welcome() {
+		return "<h1>Welcome buddy</h1>";
 	}
 
 }
